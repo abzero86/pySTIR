97aa4e5f38a28d6d5891e7c1b4fddc9803c3c8e4
abzero86 <wangzy29@qq.com>
2013-08-11 00:29:34 +0800

    updated

--- product.py
+++ product.py
@@ -4,21 +4,14 @@ class product:
         self.productName=productName
         self.previousProduct=previousProduct
         self.nextProduct=nextProduct
-		self.Asks={}
+		self.prices={askPrice:[askQty,'ASK'],bidPrice:[bidQty,'BID']}
+	def priceUpdate(self,price,qty,type):
+		self.prices.update({price:[qty,type]})
+		
 
-class outright(product):
-
-class spread(product):
-    def __init__(self,productID,productName,previousProduct,nextProduct,outrightA,outrightB):
-        product.__init__(self,productID,productName,previousProduct,nextProduct,spreadlength)
-        self.outrightA=outrightA
-        self.outrightB=outrightB
-        self.spreadLength=spreadLength
-
-class fly(product):
-    def __init__(self,productID,productName,previousProduct,nextProduct,spreadA,spreadB):
-        product.__init__(self,productID,productName,previousProduct,nextProduct)
-        self.spreadA=spreadA
-        self.spreadB=spreadB
-            
-        
+class strategy(product):
+    def __init__(self,productID,productName,previousProduct,nextProduct,askPrice,askQty,bidPrice,bidQty,legA,legB,length):
+        product.__init__(self,productID,productName,previousProduct,nextProduct,askPrice,askQty,bidPrice,bidQty)
+        self.legA=legA
+        self.legB=legB
+        self.length=length
